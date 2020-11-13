8949555484d45af5278c473443b60d8de5c4d096
Dzen77 <beldenis1977@yandex.ru>
2020-11-13 17:18:00 +0300

    Cycles 2

---
+++ src/ru/progwards/java1/lessons/classes/Animals.java
@@ -0,0 +1,10 @@
+package ru.progwards.java1.lessons.classes;
+
+public class Animals {
+    public static void printinfo(Animal animal) {
+        System.out.println(animal);
+    }
+    public static void main(String[] args) {
+        Cow x = new Cow();
+    }
+}
