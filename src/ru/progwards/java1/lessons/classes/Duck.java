f2cd064db546803eeee2f2449bf22141eb9d7518
Dzen77 <beldenis1977@yandex.ru>
2020-11-13 19:29:12 +0300

    Cycles 9

--- src/ru/progwards/java1/lessons/classes/Duck.java
+++ src/ru/progwards/java1/lessons/classes/Duck.java
@@ -3,10 +3,12 @@ package ru.progwards.java1.lessons.classes;
 public class Duck extends Animal {
         @Override
         public AnimalKind getKind() {
+            AnimalKind ani = AnimalKind.DUCK;
             return AnimalKind.DUCK;
         }
         @Override
         public FoodKind getFoodKind() {
+            FoodKind food = FoodKind.CORN;
             return FoodKind.CORN;
         }
 }
