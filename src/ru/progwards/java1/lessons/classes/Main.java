1879cc464eadb5d59623a8a908813a8026e1bef4
Dzen77 <beldenis1977@yandex.ru>
2020-11-13 18:21:24 +0300

    Cycles 5

--- src/ru/progwards/java1/lessons/classes/Main.java
+++ src/ru/progwards/java1/lessons/classes/Main.java
@@ -1,7 +1,9 @@
 package ru.progwards.java1.lessons.classes;
 
 public class Main {
+    private static Object Animal;
+
     public static void main(String[] args) {
-        System.out.println(getKind());
+        System.out.println(Animal);
     }
 }
