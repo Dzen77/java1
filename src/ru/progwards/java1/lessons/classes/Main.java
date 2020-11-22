281b98b74a85f1a65f21eda6edc01300321f58e4
Dzen77 <beldenis1977@yandex.ru>
2020-11-22 14:28:55 +0300

    Text 2

--- src/ru/progwards/java1/lessons/classes/Main.java
+++ src/ru/progwards/java1/lessons/classes/Main.java
@@ -1,20 +1,4 @@
 package ru.progwards.java1.lessons.classes;
 
 public class Main {
-    enum AnimalKind {
-        ANIMAL,
-        COW,
-        HAMSTER,
-        DUCK,
-    }
-    enum FoodKind {
-        UNKNOWN,
-        HAY,
-        CORN,
-    }
-    public static void main(String[] args) {
-        Cow cou = new Cow();
-        Hamster ham = new Hamster();
-        Duck duck = new Duck();
-    }
 }
