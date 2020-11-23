a7639d445c32b8ad6041a932610e1c516114946a
Dzen77 <beldenis1977@yandex.ru>
2020-11-23 18:08:40 +0300

    Commit 26

--- src/ru/progwards/java1/lessons/classes/ComplexNum.java
+++ src/ru/progwards/java1/lessons/classes/ComplexNum.java
@@ -7,4 +7,10 @@ public class ComplexNum {
         x = a;
         y = b;
     }
+    public String toString() {
+        String a = Integer.toString(x);
+        String b = Integer.toString(y);
+        System.out.println(a + b + "i");
+        return (a + b + "i");
+    }
 }
