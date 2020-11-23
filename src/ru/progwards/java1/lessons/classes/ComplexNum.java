de3eff35843bb9e236499e8c2727737a1e40aa26
Dzen77 <beldenis1977@yandex.ru>
2020-11-23 16:15:59 +0300

    Commit 23

--- src/ru/progwards/java1/lessons/classes/ComplexNum.java
+++ src/ru/progwards/java1/lessons/classes/ComplexNum.java
@@ -1,21 +1,4 @@
 package ru.progwards.java1.lessons.classes;
 
 public class ComplexNum {
-    public ComplexNum(int a, int b) {
-    }
-    public String toString() {
-        return;
-    }
-    public ComplexNum add(ComplexNum num) {
-        return;
-    }
-    public ComplexNum sub(ComplexNum num) {
-        return;
-    }
-    public ComplexNum mul(ComplexNum num) {
-        return;
-    }
-    public ComplexNum div(ComplexNum num) {
-        return;
-    }
 }
