6283fc48f51bcf22624637a445a6ac0606e432b9
Dzen77 <beldenis1977@yandex.ru>
2020-11-26 16:13:15 +0300

    Commit 33

--- src/ru/progwards/java1/lessons/classes/ComplexNum.java
+++ src/ru/progwards/java1/lessons/classes/ComplexNum.java
@@ -10,16 +10,20 @@ public class ComplexNum {
     public ComplexNum add(ComplexNum num) {//Метод для сложения двух комплексных чисел
         return new ComplexNum(this.a + num.a, this.b + num.b);//возвращаем сумму частей
     }
-    void printComplexNumber() {//Функция для печати комлексного номера
+    void printComplexNumber() {//Функция для печати комплексного номера
         System.out.println(this.a + "+" + this.b + "i");
     }
-    public String toString() {
+    public ComplexNum sub(ComplexNum num) {//Метод для сложения двух комплексных чисел
+        return new ComplexNum(this.a - num.a, this.b - num.b);//возвращаем сумму частей
+    }
+        public String toString() {
         return a + "+" + b + "i";
     }
     public static void main(String[] args) {
         ComplexNum C1 = new ComplexNum(1,7);
-        ComplexNum C2 = new ComplexNum(5,3);
+        ComplexNum C2 = new ComplexNum(5,2);
         System.out.println(C1.add(C2));
+        System.out.println(C1.sub(C2));
         C1.printComplexNumber();
         C2.printComplexNumber();
     }
