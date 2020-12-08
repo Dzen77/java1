98adb581e961f1389278ba139c09082abea27b17
Dzen77 <beldenis1977@yandex.ru>
2020-12-08 14:00:39 +0300

    Commit 4

--- src/ru/progwards/java1/lessons/arrays/ArraySort.java
+++ src/ru/progwards/java1/lessons/arrays/ArraySort.java
@@ -6,7 +6,7 @@ public class ArraySort {
     int[] a = {5, 3, 9, -7, 0, 1};
     public static void sort(int[] a) {
         Arrays.sort(a);
-        for (int i = 0; i < a.length; ++i) {
+        for (int i = 0; i < 5; ++i) {
             for (int j = i + 1; a[i] > a[j]; ++j) {
                 int temp = a[i];
                 a[i] = a[j];
