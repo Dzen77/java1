3d8f3da033a34017bf2c58b4b1d42075b43e428a
Dzen77 <beldenis1977@yandex.ru>
2020-12-08 16:19:17 +0300

    Commit 5

---
+++ src/ru/progwards/java1/lessons/arrays/DIntArray.java
@@ -0,0 +1,23 @@
+package ru.progwards.java1.lessons.arrays;
+
+import java.util.Arrays;
+
+public class DIntArray {
+    private int[] a = {5, 3, 9, -7, 0, 1};;
+    public DIntArray () {
+    }
+    public void add(int num) {
+        int aCopi = Arrays.copyOf(a, a.length + 1);
+        aCopi[a.length] = num;
+    }
+    public void atInsert(int pos, int num) {
+    }
+    public void atDelete(int pos) {
+    }
+    public int at(int pos) {
+        return a[pos];
+    }
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(add(10));
+    }
+}
