5c4b8b7a28f0ad5ca2a97d4b9508563ec6dd1c93
Dzen77 <beldenis1977@yandex.ru>
2020-12-08 16:42:25 +0300

    Commit 7

--- src/ru/progwards/java1/lessons/test/Test1.java
+++ src/ru/progwards/java1/lessons/test/Test1.java
@@ -1,18 +1,16 @@
 package ru.progwards.java1.lessons.test;
 
+import java.util.Arrays;
+
 public class Test1 {
-    public static int reverseDigits(int number) {
-        int c = number/100;
-        int b = (number - c * 100)/10;
-        int w = b * 10;
-        int a = (number - c * 100 - b * 10);
-        int u = a * 100;
-        int result = u + w + c;
-        return result;
-    }
     public static void main(String[] args) {
-        int number = 789;
-        int num = reverseDigits(number);
-        System.out.println(num);
+        int[] a = {5, 3, 9, -7, 0, 1};
+        System.out.println(a);
+        System.out.println(Arrays.toString(a));
+        for (int i = 0; i < 6; i++) {
+            System.out.print(a[i] + ", ");
+        }
+        System.out.println(Arrays.toString(a));
+        System.out.println(a.length);
     }
 }
