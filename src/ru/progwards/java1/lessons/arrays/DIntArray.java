cabb13e6112915f1b0bea80190653b8607731c0c
Dzen77 <beldenis1977@yandex.ru>
2020-12-09 11:43:12 +0300

    Commit 23

--- src/ru/progwards/java1/lessons/arrays/DIntArray.java
+++ src/ru/progwards/java1/lessons/arrays/DIntArray.java
@@ -22,28 +22,28 @@ public class DIntArray {
             a[0] = num;
         } else {
             int[] aCopi1 = new int[a.length + 1];
-            if (a[pos] >= 0&&a[pos] < a.length) {
+            if (a[pos] >= 0 && a[pos] < a.length) {
                 System.arraycopy(a, 0, aCopi1, 0, a[pos - 1]);
                 aCopi1[pos] = num;
-                if (a[pos] >= 0&&a[pos] < a.length) {
+                if (a[pos] >= 0 && a[pos] < a.length) {
                     System.arraycopy(a, pos + 1, aCopi1, pos + 1, aCopi1.length);
                     a = aCopi1;
                 }
             }
         }
-        public void atDelete(int pos) {
-            if (a == null) {
-                a = new int[1];
-                a[0] = pos;
-            } else {
-                int[] aCopi2 = new int[a.length - 1];
-                System.arraycopy(a, 0, aCopi2, 0, a[pos - 1]);
-                System.arraycopy(a, pos + 1, aCopi2, pos + 1, a.length);
-                a = aCopi2;
-            }
+    }
+    public void atDelete(int pos) {
+        if (a == null) {
+            a = new int[1];
+            a[0] = pos;
+        } else {
+            int[] aCopi2 = new int[a.length - 1];
+            System.arraycopy(a, 0, aCopi2, 0, a[pos - 1]);
+            System.arraycopy(a, pos + 1, aCopi2, pos + 1, a.length);
+            a = aCopi2;
         }
-        public int at(int pos){
-            return a[pos];
-        }
+    }
+    public int at(int pos) {
+        return a[pos];
     }
 }
