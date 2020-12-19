4c689f23b81bfa693056bde7cf440de957fb7945
Dzen77 <beldenis1977@yandex.ru>
2020-12-19 20:37:45 +0300

    Commit 3

--- src/ru/progwards/java1/lessons/bitsworld/SumBits.java
+++ src/ru/progwards/java1/lessons/bitsworld/SumBits.java
@@ -5,14 +5,14 @@ public class SumBits {
         int summ = 0;
         int i;
         byte x = value;
-        for (i=1;i<8;i++){
+        for (i=0;i<=7;i++){
                 summ += x & 00000001;
                 x = (byte)(x >> 1);
             }
         return summ;
     }
     public static void main(String[] args) {
-        byte value = (byte)0b01100110;
+        byte value = (byte)0b10000000;
         int result = sumBits(value);
         System.out.println(result);
         System.out.println(Integer.toBinaryString(result));
